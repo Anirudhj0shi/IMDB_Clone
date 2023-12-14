67927227bcc25f81272690bbdc628440ce191541
Anirudh joshi <yashjoshi5521@gmail.com>
2023-12-15 01:34:47 +0530

    My Clone 1

---
+++ IMDB_CLONE/src/App.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import{BrowserRouter as Router, Routes , Route } from 'react-router-dom'
+import Header from './components/header/Header'
+import Home from './pages/home/Home'
+import MovieList from './components/movieList/movieList'
+import Movie from './pages/movieDetail/movie'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='App'>
+        <Router>
+          <Header />
+          <Routes>
+            <Route index element={<Home />}></Route>
+            <Route path="movie/:id" element={<Movie/>}></Route>
+            <Route path="movies/:type" element={<MovieList />}></Route>
+            <Route path="/*" element={<h1>Error page</h1>}></Route>
+          </Routes>
+        </Router>
+      </div>
+    </>
+  )
+}
+
+export default App
